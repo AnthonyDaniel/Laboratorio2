1ce159d83308f5f7702063b3e08c87963fa97d98
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2020-03-04 19:19:06 -0600

    First Lab 2

--- app/src/main/java/com/thony/laboratorio2/MainActivity.java
+++ app/src/main/java/com/thony/laboratorio2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.thony.laboratorio2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MessageActivity.class);
+        EditText editText = findViewById(R.id.text_message);
+        intent.putExtra("MESSAGE",editText.getText().toString());
+        startActivity(intent);
     }
 }
